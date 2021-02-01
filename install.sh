1c64f9b3f97a8733f457ae937036ef81bdb76601
yuri.liang <yuri.liang@ericsson.com>
2021-02-01 22:01:48 +0800

    update install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
 #!/usr/bin/env bash
+set +e
+set +x
+
+sudo yum install -y gcc make ncurses ncurses-devel
+sudo yum install -y ctags git tcl-devel \
+ruby ruby-devel \
+lua lua-devel \
+luajit luajit-devel \
+python python-devel \
+perl perl-devel \
+perl-ExtUtils-ParseXS \
+perl-ExtUtils-XSpp \
+perl-ExtUtils-CBuilder \
+perl-ExtUtils-Embed
+
+sudo yum list installed | grep –i vim
+sudo yum remove vim-enhanced vim-common vim-filesystem
+
+sudo git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+--enable-multibyte \
+--enable-rubyinterp \
+--enable-pythoninterp \
+--enable-perlinterp \
+--enable-luainterp
+make
+sudo make install
+
+cd ..
+rm -rf vim
 
 # step 1
 git submodule update --init --remote
