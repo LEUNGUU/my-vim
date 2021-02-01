ebbd6759dba97909242f4720633d166a8506ddc0
yuri.liang <yuri.liang@ericsson.com>
2021-02-01 15:32:29 +0800

    update install scripts

---
+++ scripts/inventory.sh
@@ -0,0 +1,30 @@
+declare -A PlugStart;
+
+PlugStart=(
+'vim-airline/vim-airline' \
+'vim-airline/vim-airline-themes' \
+'w0ng/vim-hybrid' \
+'itchyny/vim-cursorword' \
+'terryma/vim-multiple-cursors' \
+'haya14busa/incsearch.vim' \
+'haya14busa/incsearch-fuzzy.vim' \
+'jszakmeister/vim-togglecursor' \
+'tpope/vim-surround' \
+'tpope/vim-commentary' \
+'rhysd/accelerated-jk' \
+'lvht/fzf' \
+'neoclide/coc.nvim' \
+'frazrepo/vim-rainbow' \
+'airblade/vim-gitgutter' \
+'romainl/vim-cool' \
+)
+
+declare -A PlugOpt;
+
+PlugOpt=(
+'pearofducks/ansible-vim' \
+'hashivim/vim-terraform' \
+'fatih/vim-go' \
+'elzr/vim-json' \
+)
+
