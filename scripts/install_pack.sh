3a3766a42eb2966f7d922785952df29e101d157e
yuri.liang <yuri.liang@ericsson.com>
2021-02-01 21:55:29 +0800

    update install scripts

--- scripts/install_pack.sh
+++ scripts/install_pack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./scripts/inventory.sh
+
 add(){
   git submodule add 'https://github.com/'$1 $2
 }
@@ -17,12 +19,12 @@ update_plugins() {
 
 install_start() {
   # delete first
-  for plug in $(ls pack/vendor/start);do
-    if [[ ! " ${PlugStart[@]} " =~ " ${plug} " ]]; then
-      delete pack/vendor/start/${plug}
-    fi
-  done
-  for plug in ${PlugStart[@]};do
+#  for plug in $(ls pack/vendor/start);do
+#    if [[ ! " ${PlugStart[@]} " =~ " ${plug} " ]]; then
+#      delete pack/vendor/start/${plug}
+#    fi
+#  done
+  for plug in ${Plugins[start]};do
     plugname=$(echo ${plug} | cut -d/ -f2)
     add ${plug} pack/vendor/start/${plugname}
   done
@@ -30,13 +32,13 @@ install_start() {
 
 install_opt() {
   # delete first
-  for plug in $(ls pack/vendor/opt);do
-    # if the plugin not in the list of Opt, then delete it
-    if [[ ! " ${PlugOpt[@]} " =~ " ${plug} " ]]; then
-      delete pack/vendor/opt/${plug}
-    fi
-  done
-  for plug in ${PlugOpt[@]};do
+#  for plug in $(ls pack/vendor/opt);do
+#    # if the plugin not in the list of Opt, then delete it
+#    if [[ ! " ${PlugOpt[@]} " =~ " ${plug} " ]]; then
+#      delete pack/vendor/opt/${plug}
+#    fi
+#  done
+  for plug in ${Plugins[opt]};do
      plugname=$(echo ${plug} | cut -d/ -f2)
      add ${plug} pack/vendor/opt/${plugname}
   done
--- scripts/inventory.sh
+++ scripts/inventory.sh
@@ -1,30 +1,6 @@
-declare -A PlugStart;
+declare -A Plugins;
 
-PlugStart=(
-'vim-airline/vim-airline' \
-'vim-airline/vim-airline-themes' \
-'w0ng/vim-hybrid' \
-'itchyny/vim-cursorword' \
-'terryma/vim-multiple-cursors' \
-'haya14busa/incsearch.vim' \
-'haya14busa/incsearch-fuzzy.vim' \
-'jszakmeister/vim-togglecursor' \
-'tpope/vim-surround' \
-'tpope/vim-commentary' \
-'rhysd/accelerated-jk' \
-'lvht/fzf' \
-'neoclide/coc.nvim' \
-'frazrepo/vim-rainbow' \
-'airblade/vim-gitgutter' \
-'romainl/vim-cool' \
-)
+Plugins[start]="vim-airline/vim-airline vim-airline/vim-airline-themes w0ng/vim-hybrid itchyny/vim-cursorword terryma/vim-multiple-cursors haya14busa/incsearch.vim haya14busa/incsearch-fuzzy.vim jszakmeister/vim-togglecursor tpope/vim-surround tpope/vim-commentary rhysd/accelerated-jk lvht/fzf neoclide/coc.nvim frazrepo/vim-rainbow airblade/vim-gitgutter romainl/vim-cool";
 
-declare -A PlugOpt;
-
-PlugOpt=(
-'pearofducks/ansible-vim' \
-'hashivim/vim-terraform' \
-'fatih/vim-go' \
-'elzr/vim-json' \
-)
+Plugins[opt]="pearofducks/ansible-vim hashivim/vim-terraform fatih/vim-go elzr/vim-json";
 
